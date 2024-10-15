6e304138809d76b39952548f9456eced43fe70fb
courage <asphyxia123@protonmail.com>
2024-10-15 21:17:57 +0200

    initial

--- bencode_parser.hpp
+++ bencode_parser.hpp
@@ -12,15 +12,61 @@
 #define BENCODE_PARSER_HPP
 
 // define bencode_val using a struct to allow recursive definition
-using bencode_val = std::variant<
-    int64_t,                               // integer
-    std::vector<uint8_t>,                  // byte string
-    std::vector<bencode_val>,              // list (vector of bencode_val)
-    std::map<std::string, bencode_val*>     // dictionary (map of string to bencode_val)
->;
 
-class Bencode {
-public:
+struct BencodeData {
+    std::variant<
+        int,
+        std::vector<uint8_t>,
+        std::unique_ptr<std::vector<BencodeData>>,
+        std::map<std::string, std::unique_ptr<BencodeData>>
+    > value;
 };
+
+class BencodeParser {
+public:
+    const std::string data;
+    size_t idx = 0;
+
+    char peek() const {
+        return data[idx];
+    }
+
+    char consume() {
+        return data[idx++];
+    }
+
+    BencodeParser(std::string data) : data(std::move(data)) {
+    }
+
+    BencodeData parse() {
+        char ch = peek();
+        if (ch == 'i') return parse_integer();
+        if (ch == 'l') return parse_list();
+        if (ch == 'd') return parse_dict();
+        if (isdigit(ch)) return parse_string();
+    }
+
+private:
+    BencodeData parse_integer() {
+        consume();
+        while (true) {
+
+        }
+    }
+
+    BencodeData parse_list() {
+
+    }
+
+    BencodeData parse_dict() {
+
+    }
+
+    BencodeData parse_string() {
+
+    }
+};
+
+
 
 #endif //BENCODE_PARSER_HPP
